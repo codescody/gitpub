e5cbf64f9302a0abbb830911b8d7ff26d0a188e2
Cody Clark <codylcl@yahoo.com>
2022-10-11 18:32:38 -1000

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const drinks = require('./models/drinks')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Gitpub App!')
+})
+
+app.get('/drinks', (req, res) => {
+    res.send(drinks)
+})
+
+app.listen(3000, () => {
+    console.log('listening')
+})
