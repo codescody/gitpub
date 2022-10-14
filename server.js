b6c8110b472aeb2648709ed39c181f93bfc3d5aa
Cody Clark <codylcl@yahoo.com>
2022-10-13 16:05:05 -1000

    food index and show pages

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const drinks = require('./models/drinks')
+const food = require('./models/food')
 
 const app = express()
 
@@ -13,9 +14,23 @@ app.get('/drinks', (req, res) => {
     })
 })
 
+
 app.get('/drinks/:id', (req, res) => {
     res.render(`drinks_show.ejs`, {
         drinks: drinks[req.params.id],
+
+    })
+})
+
+app.get('/food', (req, res) => {
+    res.render(`food_index.ejs`, {
+        allFood: food
+    })
+})
+
+app.get('/food/:id', (req, res) => {
+    res.render(`food_show.ejs`, {
+        food: food[req.params.id],
 
     })
 })
